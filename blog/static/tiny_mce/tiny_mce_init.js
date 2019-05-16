e77d9fac36baf2176afb0636e29ef47408b33bef
zubogain <zubogain@gmail.com>
2019-05-16 18:27:39 +0600

    1.1.2-alpha - fixed gitignore file: previously all static directories were ignored when loading into git. Because of this, styles and scripts were not loaded when the page was loaded

---
+++ blog/static/tiny_mce/tiny_mce_init.js
@@ -0,0 +1,13 @@
+tinyMCE.init({
+    mode:"textareas",
+    language : "ru",
+    plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+    // Theme options
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,sub,sup,|,forecolor,backcolor,formatselect,fontsizeselect",
+    theme_advanced_buttons2 : "outdent,indent,|,undo,redo,|,link,unlink,anchor,image,tablecontrols,removeformat,code",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true
+});
